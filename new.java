29ae8e47619fcba86ce8c7f9a840b3204e14ca93
vishal <vishal>
2024-01-09 07:17:59 +0000

    add the line

--- new.java
+++
@@ -1,9 +0,0 @@
-<<<<<<< HEAD
-this is the code written in the master first line of the code
-
-second line of the code 
-=======
-this is the code wirtten in the issue1 first line of code
-
-second line of code
->>>>>>> issue1
